7bba7438d028ed674f5fcafa808cb73f43780f11
Alex Schwab <alexschwab@users.noreply.github.com>
2015-08-27 22:42:14 -0700

    clock face complete
    still want to pull weather data and put that on the left

--- src/face_handler.c
+++ src/face_handler.c
@@ -4,6 +4,7 @@
 //-- static globals --
 static Window *s_window;
 static GFont s_res_hoog_numbers;
+static GFont s_res_hoog_numbers_36;
 static GFont s_res_scp;
 static TextLayer *s_time_hr;
 static TextLayer *s_time_hr2;
@@ -11,14 +12,19 @@ static TextLayer *s_time_min;
 static TextLayer *s_time_min2;
 static TextLayer *s_ampm;
 static TextLayer *s_date_year;
+static TextLayer *s_date_year2;
 static TextLayer *s_date_day;
 static TextLayer *s_date_month;
-static TextLayer *s_day;
-static TextLayer *s_month;
+static TextLayer *s_day_v;
+static TextLayer *s_date_day_v;
+static TextLayer *s_month_v;
+static TextLayer *s_year_v;
 static TextLayer *s_battery;
 
 #define SPACE 32;
-
+#define Blu    GColorFromHEX(0x4FA2FE);
+#define Green  GColorFromHEX(0x006A66);
+#define Orange GColorFromHEX(0xDC571F);
 //-- functions --
 
 static void update_daily(void)
@@ -26,34 +32,47 @@ static void update_daily(void)
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
   
-  static char date_year[]  = "2015";
+  static char date_year[]  = "20";
+  static char date_year2[] = "15";
   static char date_day[]   = "01";
   static char date_month[] = "01";
-  static char day[]        = "F\nR\nI";
-  static char month[]      = "J\nA\nN"; // vertical month
-  
-  strftime(date_year,  sizeof(date_year),  "%G", tick_time);
+  static char day_v[]      = "F\nR\nI";
+  static char date_day_v[] = "0\n1";
+  static char month_v[]    = "J\nA\nN";
+  static char year_v[]     = "2\n0\n1\n5";
+                            //0 12 34 56
   strftime(date_day,   sizeof(date_day),   "%d", tick_time);
+  date_day_v[0] = date_day[0]; date_day_v[2] = date_day[1];
   strftime(date_month, sizeof(date_month), "%m", tick_time);
   
   {
-    char buffer[4];
-    strftime(buffer, sizeof(buffer), "%b", tick_time);
-    month[0] = buffer[0];
-    month[2] = buffer[1] - SPACE;
-    month[4] = buffer[2] - SPACE;
+    char buffer[5];
     
-    strftime(buffer, sizeof(day), "%a", tick_time);
-    day[0] = buffer[0];
-    day[2] = buffer[1] - SPACE;
-    day[4] = buffer[2] - SPACE;
+    strftime(buffer,  sizeof(buffer),  "%G", tick_time);
+    date_year[0]  = year_v[0] = buffer[0];
+    date_year[1]  = year_v[2] = buffer[1];
+    date_year2[0] = year_v[4] = buffer[2];
+    date_year2[1] = year_v[6] = buffer[3];
+    
+    strftime(buffer, sizeof(buffer), "%b", tick_time);
+    month_v[0] = buffer[0];
+    month_v[2] = buffer[1] - SPACE;
+    month_v[4] = buffer[2] - SPACE;
+    
+    strftime(buffer, sizeof(buffer), "%a", tick_time);
+    day_v[0] = buffer[0];
+    day_v[2] = buffer[1] - SPACE;
+    day_v[4] = buffer[2] - SPACE;
   }
   
   text_layer_set_text(s_date_year, date_year);
+  text_layer_set_text(s_date_year2, date_year2);
   text_layer_set_text(s_date_day, date_day);
   text_layer_set_text(s_date_month, date_month);
-  text_layer_set_text(s_day, day);
-  text_layer_set_text(s_month, month);
+  text_layer_set_text(s_day_v, day_v);
+  text_layer_set_text(s_date_day_v, date_day_v);
+  text_layer_set_text(s_month_v, month_v);
+  text_layer_set_text(s_year_v, year_v);
 }
 
 static void update_minute(void)
@@ -88,17 +107,16 @@ static void update_minute(void)
 
 static void battery_handler(BatteryChargeState charge_state)
 {
-  //                        0    1    2    3    4     5
-  static char battery[] = {' ', '1', '0', '0', '%', '\0'};
+  static char battery[] = " 100%";
   if(charge_state.charge_percent == 100) // full charge
   {
-    battery[1] = '1';
-    battery[2] = '0';
+     battery[1] = '1';
+     battery[2] = '0';
   }
   else
   {
     battery[1] = ' ';
-    battery[2] = (char)(charge_state.charge_percent / 10) + '0';
+    battery[2] = '0' + charge_state.charge_percent / 10;
   }
   
   if(charge_state.is_plugged)
@@ -111,7 +129,7 @@ static void battery_handler(BatteryChargeState charge_state)
       battery[2] = '7';
     }
   }
-  else
+  else // not plugged in (or charging)
     battery[0] = ' ';
   
   text_layer_set_text(s_battery, battery);
@@ -139,90 +157,115 @@ static void initialise_ui(void) {
   
   //-- load font resources --
   s_res_hoog_numbers = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_hoog_numbers_80));
+  s_res_hoog_numbers_36 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_hoog_numbers_36));
   s_res_scp = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_SCP_12));
-  
+  //-- colors --
+  GColor8 font_color_pri = Blu;
+  GColor8 font_color_sec = GColorWhite;
+  GColor8 font_color_ter = GColorWhite;
   //-- text layers --
   // s_time_hr
-  s_time_hr = text_layer_create(GRect(50, -15, 50, 90));
+  s_time_hr = text_layer_create(GRect(50, -20, 50, 90));
   text_layer_set_background_color(s_time_hr, GColorClear);
-  text_layer_set_text_color(s_time_hr, GColorWhite);
+  text_layer_set_text_color(s_time_hr, font_color_pri);
   text_layer_set_text_alignment(s_time_hr, GTextAlignmentRight);
   text_layer_set_font(s_time_hr, s_res_hoog_numbers);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_time_hr);
   
   // s_time_hr2
-  s_time_hr2 = text_layer_create(GRect(50, -15, 100, 90));
+  s_time_hr2 = text_layer_create(GRect(50, -20, 100, 90));
   text_layer_set_background_color(s_time_hr2, GColorClear);
-  text_layer_set_text_color(s_time_hr2, GColorWhite);
+  text_layer_set_text_color(s_time_hr2, font_color_pri);
   text_layer_set_text_alignment(s_time_hr2, GTextAlignmentRight);
   text_layer_set_font(s_time_hr2, s_res_hoog_numbers);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_time_hr2);
   
   // s_time_min
-  s_time_min = text_layer_create(GRect(50, 40, 50, 90));
+  s_time_min = text_layer_create(GRect(50, 35, 50, 90));
   text_layer_set_background_color(s_time_min, GColorClear);
-  text_layer_set_text_color(s_time_min, GColorWhite);
+  text_layer_set_text_color(s_time_min, font_color_pri);
   text_layer_set_text_alignment(s_time_min, GTextAlignmentRight);
   text_layer_set_font(s_time_min, s_res_hoog_numbers);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_time_min);
   
   // s_time_min2
-  s_time_min2 = text_layer_create(GRect(50, 40, 100, 90));
+  s_time_min2 = text_layer_create(GRect(50, 35, 100, 90));
   text_layer_set_background_color(s_time_min2, GColorClear);
-  text_layer_set_text_color(s_time_min2, GColorWhite);
+  text_layer_set_text_color(s_time_min2, font_color_pri);
   text_layer_set_text_alignment(s_time_min2, GTextAlignmentRight);
   text_layer_set_font(s_time_min2, s_res_hoog_numbers);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_time_min2);
   
-  // s_ampm
-  s_ampm = text_layer_create(GRect(50, 0, 16, 12));
-  text_layer_set_background_color(s_ampm, GColorClear);
-  text_layer_set_text_color(s_ampm, GColorWhite);
-  text_layer_set_font(s_ampm, s_res_scp);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_ampm);
-  
-  // s_date_year = eg. 2015
-  s_date_year = text_layer_create(GRect(0, 148, 28, 12));
+  // s_date_year = first part of a year (20)
+  s_date_year = text_layer_create(GRect(50, 130, 46, 37));
   text_layer_set_background_color(s_date_year, GColorBlack);
-  text_layer_set_text_color(s_date_year, GColorWhite);
-  text_layer_set_text_alignment(s_date_year, GTextAlignmentCenter);
-  text_layer_set_font(s_date_year, s_res_scp);
+  text_layer_set_text_color(s_date_year, font_color_sec);
+  text_layer_set_font(s_date_year, s_res_hoog_numbers_36);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_date_year);
   
+  // s_date_year2 = last part of a year (15)
+  s_date_year2 = text_layer_create(GRect(100, 130, 46, 37));
+  text_layer_set_background_color(s_date_year2, GColorBlack);
+  text_layer_set_text_color(s_date_year2, font_color_sec);
+  text_layer_set_text_alignment(s_date_year2, GTextAlignmentRight);
+  text_layer_set_font(s_date_year2, s_res_hoog_numbers_36);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_date_year2);
+  
   // s_date_day = 01, 02, 03, ..., 31
-  s_date_day = text_layer_create(GRect(0, 136, 28, 12));
+  s_date_day = text_layer_create(GRect(99, 105, 46, 37));
   text_layer_set_background_color(s_date_day, GColorClear);
-  text_layer_set_text_color(s_date_day, GColorWhite);
+  text_layer_set_text_color(s_date_day, font_color_sec);
   text_layer_set_text_alignment(s_date_day, GTextAlignmentRight);
-  text_layer_set_font(s_date_day, s_res_scp);
+  text_layer_set_font(s_date_day, s_res_hoog_numbers_36);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_date_day);
   
   // s_date_month = 01, 02, 03, ..., 12
-  s_date_month = text_layer_create(GRect(0, 136, 28, 12));
+  s_date_month = text_layer_create(GRect(50, 105, 46, 37));
   text_layer_set_background_color(s_date_month, GColorClear);
-  text_layer_set_text_color(s_date_month, GColorWhite);
-  text_layer_set_font(s_date_month, s_res_scp);
+  text_layer_set_text_color(s_date_month, font_color_sec);
+  text_layer_set_text_alignment(s_date_month, GTextAlignmentRight);
+  text_layer_set_font(s_date_month, s_res_hoog_numbers_36);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_date_month);
   
-  // s_day = vertical day listing
-  s_day = text_layer_create(GRect(40, 12, 24, 36));
-  text_layer_set_background_color(s_day, GColorClear);
-  text_layer_set_text_color(s_day, GColorWhite);
-  text_layer_set_font(s_day, s_res_scp);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_day);
+  // s_ampm
+  s_ampm = text_layer_create(GRect(50, -4, 14, 14));
+  text_layer_set_background_color(s_ampm, GColorClear);
+  text_layer_set_text_color(s_ampm, font_color_ter);
+  text_layer_set_font(s_ampm, s_res_scp);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_ampm);
   
-  // s_month = vertical month listing
-  s_month = text_layer_create(GRect(40, 67, 24, 36));
-  text_layer_set_background_color(s_month, GColorClear);
-  text_layer_set_text_color(s_month, GColorWhite);
-  text_layer_set_font(s_month, s_res_scp);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_month);
-    
+  // s_day_v = vertical day listing
+  s_day_v = text_layer_create(GRect(40, 7, 8, 36));
+  text_layer_set_background_color(s_day_v, GColorClear);
+  text_layer_set_text_color(s_day_v, font_color_ter);
+  text_layer_set_font(s_day_v, s_res_scp);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_day_v);
+  
+  // s_date_day_v = same as s_date_day, but vertical, below s_day
+  s_date_day_v = text_layer_create(GRect(40, 48, 8, 24));
+  text_layer_set_background_color(s_date_day_v, GColorClear);
+  text_layer_set_text_color(s_date_day_v, font_color_ter);
+  text_layer_set_font(s_date_day_v, s_res_scp);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_date_day_v);
+  
+  // s_month_v = vertical month listing
+  s_month_v = text_layer_create(GRect(40, 75, 8, 36));
+  text_layer_set_background_color(s_month_v, GColorClear);
+  text_layer_set_text_color(s_month_v, font_color_ter);
+  text_layer_set_font(s_month_v, s_res_scp);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_month_v);
+  
+  // s_year_v = vertical year listing
+  s_year_v = text_layer_create(GRect(40, 115, 8, 48));
+  text_layer_set_background_color(s_year_v, GColorClear);
+  text_layer_set_text_color(s_year_v, font_color_ter);
+  text_layer_set_font(s_year_v, s_res_scp);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_year_v);
+  
   // s_battery
-  s_battery = text_layer_create(GRect(92, 0, 48, 12));
+  s_battery = text_layer_create(GRect(105, -4, 36, 13));
   text_layer_set_background_color(s_battery, GColorClear);
-  text_layer_set_text_color(s_battery, GColorWhite);
-  text_layer_set_text_alignment(s_battery, GTextAlignmentRight);
+  text_layer_set_text_color(s_battery, font_color_ter);
   text_layer_set_font(s_battery, s_res_scp);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_battery);
   
@@ -240,12 +283,16 @@ static void destroy_ui(void) {
   text_layer_destroy(s_time_min2);
   text_layer_destroy(s_ampm);
   text_layer_destroy(s_date_year);
+  text_layer_destroy(s_date_year2);
   text_layer_destroy(s_date_day);
   text_layer_destroy(s_date_month);
-  text_layer_destroy(s_day);
-  text_layer_destroy(s_month);
+  text_layer_destroy(s_day_v);
+  text_layer_destroy(s_date_day_v);
+  text_layer_destroy(s_month_v);
+  text_layer_destroy(s_year_v);
   text_layer_destroy(s_battery);
   fonts_unload_custom_font(s_res_hoog_numbers);
+  fonts_unload_custom_font(s_res_hoog_numbers_36);
   fonts_unload_custom_font(s_res_scp);
 }
 // END AUTO-GENERATED UI CODE
